ab95dd0b95f51667cd14e3c512944d4bc944a86c
Muhammad Haris Awan <160159398+harisawan27@users.noreply.github.com>
2024-04-19 17:26:55 +0500

    first

---
+++ todo-list/index.ts
@@ -0,0 +1,29 @@
+import inquirer from "inquirer";
+
+let todo = [];
+let condition = true;
+
+while(condition)
+{
+    let addTask = await inquirer.prompt([
+    {
+        name:"todo",
+        type:"input",
+        message:"What do you want to add in your Todo list?"
+    },
+    {
+        name:"addMore",
+        type:"confirm",
+        message:"Do you want to add more tasks?",
+        default:"true"
+    }
+]);
+todo.push(addTask.todo);
+condition = addTask.addMore
+}
+
+console.log("Your todo list is:");
+
+for (let i in todo ) {
+    console.log(`=> ${i}`);
+}
