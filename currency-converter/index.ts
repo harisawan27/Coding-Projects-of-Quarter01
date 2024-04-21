a97c9f12458464f273a6e24b08dfe176916ea130
Muhammad Haris Awan <160159398+harisawan27@users.noreply.github.com>
2024-04-21 17:47:59 +0500

    first

---
+++ currency-converter/index.ts
@@ -0,0 +1,41 @@
+import inquirer from "inquirer";
+
+const currency: any = {
+    USD: 1,
+    EUR: 0.94,
+    GBP: 0.81,
+    INR: 83.37,
+    PKR: 277.69
+}
+
+console.log("*** Welcome to Currency Converter ***");
+
+
+let userAnswer = await inquirer.prompt([
+    {
+        name: "from",
+        message: "From Currency:",
+        type: "list",
+        choices:["USD", "EUR", "GBP", "INR", "PKR"]
+    },
+    {
+        name: "to",
+        message: "To Currency:",
+        type: "list",
+        choices:["USD", "EUR", "GBP", "INR", "PKR"]
+    },
+    {
+        name: "amount",
+        message: "Enter Your Amount:",
+        type: "number"
+    }
+]);
+
+let fromAmount = currency[userAnswer.from]
+let toAmount = currency[userAnswer.to]
+let amount = userAnswer.amount
+let baseAmount = amount / fromAmount
+let convertedAmount = baseAmount * toAmount
+
+console.log("Your Converted Amount is: " + convertedAmount);
+
